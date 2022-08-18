6d1604d8e51f6ce67d03632634015a87fa6ef509
Brian Williams <heffergod@gmail.com>
2022-08-18 12:59:52 -0700

    Part II continued

--- arrays.js
+++ arrays.js
@@ -2,3 +2,35 @@
     In this file, we'll be continuing our story
     with Jon being allowed to flee his fight with Jamie.
 */
+
+let backpack = []
+
+
+backpack.push("Jedi Robe")
+backpack.push('Lightsaber')
+backpack.push('Crippling sense of failure and guilt')
+
+backpack.splice(1,1)
+
+backpack.push('Wookie Coat')
+
+backpack.pop()
+
+backpack.push('blue milk', 'baby yoda', 'a child named luke', "com device", 'high ground', 'blaster')
+
+let stroller = [backpack.splice(3,2)]
+
+console.log(backpack)
+console.log(stroller)
+
+console.log(backpack.length)
+
+// for (i =0; i < backpack.length; i++)
+// {
+//     console.log(backpack[i])
+// }
+
+for (i in backpack)
+{
+    console.log(backpack[i])
+}
